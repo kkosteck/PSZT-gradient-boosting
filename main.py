e19748da213e67d05b72cc1dae75a70e047be857
Kacper Kostecki <48053724+kkosteck@users.noreply.github.com>
2021-01-25 00:31:58 +0100

    tree problems

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
 import pandas as pd
 import numpy as np
+import statistics
 import tree
+from scipy.optimize import fmin
 
 ROWS = 506
+M = 100
+Y_ID = 13
+TREE_DEPTH = 3
+
+def loss_function(y, x):
+    return (0.5 * (y - x) ** 2)
+
+def find_prediction(observed):
+    return statistics.mean(observed)
+
+def load_data(filename):
+    data = pd.read_csv(filename, delim_whitespace=' ', header=None)
+    return data.values.tolist()
 
 
+data = load_data('housing.csv')
 
-x = pd.read_csv('housing.csv', delim_whitespace=' ', header=None, usecols=[0,1,2,3,4,5,6,7,8,9,10,11,12], nrows=round(ROWS * 0.75))
-y = pd.read_csv('housing.csv', delim_whitespace=' ', header=None, usecols=[13], nrows=round(ROWS * 0.75))
+length = round(0.75 * len(data))
+x_train = data[:length]
+x_test = data[length:]
 
-x_t = pd.read_csv('housing.csv', delim_whitespace=' ', header=None, usecols=[0,1,2,3,4,5,6,7,8,9,10,11,12],skiprows=round(ROWS * 0.75))
-y_t = pd.read_csv('housing.csv', delim_whitespace=' ', header=None, usecols=[13],skiprows=round(ROWS * 0.75))
+y_train = list()
+y_test = list()
+print(x_train[0][1])
+# remove y from x data
+[y_train.append(j.pop(Y_ID)) for j in x_train]
+[y_test.append(j.pop(Y_ID)) for j in x_test]
 
+f_m = statistics.mean(y_train)
+residuals = list();
+
+for m in range(1, M):
+    r = list()
+    for i in range(0, len(y_train)):
+        r.append(y_train[i] - f_m)
+        decision_tree = tree.build_tree(x_train, y_train, TREE_DEPTH)
+    
--- tree.py
+++ tree.py
@@ -1,8 +1,8 @@
 import statistics
 
 class Leaf:
-    def __init__(self, data):
-        self.value = statistics.mean(data)
+    def __init__(self, y):
+        self.value = statistics.mean(y)
 
 class Decision_Node:
     def __init__(self, question, true_branch, false_branch):
@@ -15,57 +15,61 @@ class Question:
         self.column = column
         self.value = value
 
-    def match(self, example):
-        val = example[self.column]
+    def match(self, x):
+        val = x[self.column]
 
         return val >= self.value
 
-def find_best_split(data):
+def find_best_split(x, y):
     best_value = float("inf")
     best_question = None 
-    n_features = len(data[0]) - 1
+    n_features = len(x[0])
+    print(n_features)
 
     for col in range(n_features):  # for each feature
-        current_value, index = best_threshold(data[col]) 
-        question = Question(col, data[col][index])
+        current_value, index = best_threshold(x, y) 
+        question = Question(col, x[index][col])
         if current_value < best_value: 
             best_value = current_value
             best_question = question
 
     return best_question
 
-def best_threshold(data):
+def best_threshold(x, y):
     min_sum = float("inf")
     index = 0
-    for i in range(data):
+    for i in range(1, len(x)-1):
         current_sum = 0
         for j in range(0, i):
-            current_sum +=predicted_value[j]
+            current_sum +=y[j]
         if current_sum < min_sum: 
             min_sum = current_sum
-            index = j
+            index = i
     return min_sum, index
 
-def build_tree(data, depth, predicted_value):
-    question = find_best_split(data)
+def build_tree(x, y, depth):
+
+    question = find_best_split(x, y)
 
     if depth == 0:
-        return Leaf(data)
+        return Leaf(y)
     depth -= 1
 
-    true_data, false_data = partition(data, question)
+    true_x, true_y, false_x, false_y = partition(x, y, question)
 
-    true_branch = build_tree(true_data, depth, predicted_value)
+    true_branch = build_tree(true_x, true_y, depth)
 
-    false_branch = build_tree(false_data, depth, predicted_value)
+    false_branch = build_tree(false_x, false_y, depth)
 
     return Decision_Node(question, true_branch, false_branch)
 
-def partition(data, question):
-    true_data, false_data = [], []
-    for row in data:
-        if question.match(row):
-            true_rows.append(row)
+def partition(x, y, question):
+    true_x, true_y, false_x, false_y = [], [], [], []
+    for i in range(0, len(x)):
+        if question.match(x[i]):
+            true_x.append(x[i])
+            true_y.append(y[i])
         else:
-            false_rows.append(row)
-    return true_data, false_data
+            false_x.append(x[i])
+            false_y.append(y[i])
+    return true_x, true_y, false_x, false_y
