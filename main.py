d38e9032c3f543f52a1c40513ad14af88cfe0287
Kacper Kostecki <48053724+kkosteck@users.noreply.github.com>
2021-01-25 13:58:28 +0100

    Main algorithm seems to work properly
    Fix issues with tree building.
    Implementation of core equations of gradient boosting algorithm

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import statistics
 import tree
 from scipy.optimize import fmin
 
-ROWS = 506
-M = 100
-Y_ID = 13
+M = 50
 TREE_DEPTH = 3
+LEARNING_RATE = 0.1
 
 def loss_function(y, x):
     return (0.5 * (y - x) ** 2)
@@ -19,26 +18,41 @@ def load_data(filename):
     data = pd.read_csv(filename, delim_whitespace=' ', header=None)
     return data.values.tolist()
 
-
 data = load_data('housing.csv')
-
 length = round(0.75 * len(data))
+
 x_train = data[:length]
 x_test = data[length:]
 
 y_train = list()
 y_test = list()
-print(x_train[0][1])
 # remove y from x data
-[y_train.append(j.pop(Y_ID)) for j in x_train]
-[y_test.append(j.pop(Y_ID)) for j in x_test]
+[y_train.append(j.pop(len(data[0])-1)) for j in x_train]
+[y_test.append(j.pop(len(data[0])-1)) for j in x_test]
 
-f_m = statistics.mean(y_train)
-residuals = list();
+trees = list(); # list for all generated trees
 
-for m in range(1, M):
+f_0 = statistics.mean(y_train) # first predicted value
+f_m = [f_0] * len(y_train) # predicted values
+
+for m in range(1, M): # Iteration loop
+    print(m)
     r = list()
-    for i in range(0, len(y_train)):
-        r.append(y_train[i] - f_m)
-        decision_tree = tree.build_tree(x_train, y_train, TREE_DEPTH)
+    for i in range(0, len(y_train)): # Residuals for every data row loop
+        r.append(y_train[i] - f_m[i])
     
+    decision_tree = tree.build_tree(x_train, r, TREE_DEPTH) # create current decision tree
+    trees.append(decision_tree) # save decision tree
+    for i in range(0, len(f_m)): # update predicted values
+        f_m[i] += LEARNING_RATE * tree.find_value(decision_tree, x_train[i])
+
+test_predicted = [f_0] * len(y_test) # first value of predicted
+errors = [0] * len(y_test)
+
+for m_tree in trees: # add predicted values from every tree
+    for i in range(0, len(test_predicted)):
+        test_predicted[i] += LEARNING_RATE * tree.find_value(m_tree, x_train[i])
+        errors[i] = abs(y_test[i] - test_predicted[i])
+
+round_errors = [round(num, 2) for num in errors]
+print(round_errors)
--- tree.py
+++ tree.py
@@ -24,7 +24,6 @@ def find_best_split(x, y):
     best_value = float("inf")
     best_question = None 
     n_features = len(x[0])
-    print(n_features)
 
     for col in range(n_features):  # for each feature
         current_value, index = best_threshold(x, y) 
@@ -36,29 +35,38 @@ def find_best_split(x, y):
     return best_question
 
 def best_threshold(x, y):
-    min_sum = float("inf")
-    index = 0
-    for i in range(1, len(x)-1):
-        current_sum = 0
-        for j in range(0, i):
-            current_sum +=y[j]
-        if current_sum < min_sum: 
-            min_sum = current_sum
+    lowest_sum = float('inf')
+    index = None
+    for i in range(1, len(x)):
+        avg_before_threshold = statistics.mean(y[:i])
+        avg_after_threshold = statistics.mean(y[i:])
+        sum_of_residuals = 0
+        for j in range(0, len(x)):
+            if j < i:
+                sum_of_residuals += ((y[j] - avg_before_threshold) ** 2)
+            else:
+                sum_of_residuals += ((y[j] - avg_after_threshold) ** 2)
+
+        if sum_of_residuals < lowest_sum: 
+            lowest_sum = sum_of_residuals
             index = i
-    return min_sum, index
+    
+    return lowest_sum, index
+
+
+
 
 def build_tree(x, y, depth):
-
-    question = find_best_split(x, y)
-
-    if depth == 0:
+    if depth == 0 or len(y) < 2:
         return Leaf(y)
     depth -= 1
 
+    question = find_best_split(x, y)
     true_x, true_y, false_x, false_y = partition(x, y, question)
+    if not true_x or not false_x:
+        return Leaf(y)
 
     true_branch = build_tree(true_x, true_y, depth)
-
     false_branch = build_tree(false_x, false_y, depth)
 
     return Decision_Node(question, true_branch, false_branch)
@@ -73,3 +81,10 @@ def partition(x, y, question):
             false_x.append(x[i])
             false_y.append(y[i])
     return true_x, true_y, false_x, false_y
+
+def find_value(root, data_row):
+    if isinstance(root, Leaf):
+        return root.value
+    if root.question.match(data_row):
+        return find_value(root.true_branch, data_row)
+    return find_value(root.false_branch, data_row)
