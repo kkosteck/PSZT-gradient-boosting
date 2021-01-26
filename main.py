527097d0f60044f21960b89e5e0c9b31205f28ac
lo7czek <48222282+lo7czek@users.noreply.github.com>
2021-01-26 18:33:21 +0100

    Final fixes

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import statistics
 import tree
 import timeit
 from sklearn.ensemble import GradientBoostingRegressor 
-from sklearn.model_selection import train_test_split 
+from sklearn.metrics import mean_squared_error as MSE 
 from sklearn import datasets 
 import matplotlib.pyplot as plt
 
-M = 50
+M = 400
 MAX_LEAVES = 20
 TREE_DEPTH = 4
-LEARNING_RATE = 0.1
-K_FOLD = 5
+LEARNING_RATE = 0.2
+K_FOLD = 4
 FILENAME = 'housing.csv'
 
 def loss_function(y, x):
@@ -48,7 +48,7 @@ def split_data(x, y, k):
     return x_train, y_train, x_test, y_test
 
 def sklearn_gradient_boosting(x_train, y_train, x_test, y_test):
-    gbr = GradientBoostingRegressor(n_estimators = M, max_depth = TREE_DEPTH) 
+    gbr = GradientBoostingRegressor(n_estimators = M, max_depth = TREE_DEPTH, learning_rate=LEARNING_RATE) 
     gbr.fit(x_train, y_train) 
     pred_y = gbr.predict(x_test) 
 
@@ -90,6 +90,15 @@ def gradient_boosting(x_train, y_train, x_test, y_test):
 
     return test_predicted
 
+def calculate_parameters(observed, predicted):
+    rmse = round(MSE(observed, predicted) ** (1 / 2), 3)
+    minimum = round(min(predicted), 3)
+    maximum = round(max(predicted), 3)
+    median = round(statistics.median(predicted), 3)
+    mean = round(statistics.mean(predicted), 3)
+
+    return [rmse, minimum, maximum, median, mean]
+
 def main():
     x, y = load_data()
 
@@ -97,19 +106,21 @@ def main():
     predictions = []
     y_tests = []
     for k in range(0, K_FOLD):
+        print('GBR - iteration: ' + str(k+1))
         x_train, y_train, x_test, y_test = split_data(x, y, k)
         predictions.append(gradient_boosting(x_train, y_train, x_test, y_test))
         y_tests.append(y_test)
     end = timeit.default_timer() # timestamp
-    execution_time = round(end - start, 4)
+    execution_time = round(end - start, 3)
 
     start = timeit.default_timer() # timestamp
     sk_predictions = []
     for k in range(0, K_FOLD):
+        print('sklearn - iteration: ' + str(k+1))
         x_train, y_train, x_test, y_test = split_data(x, y, k)
         sk_predictions.append(sklearn_gradient_boosting(x_train, y_train, x_test, y_test))
     end = timeit.default_timer() # timestamp
-    sk_execution_time = round(end - start, 4)
+    sk_execution_time = round(end - start, 3)
 
     # average
     prediction = [0] * len(predictions[0])
@@ -127,15 +138,41 @@ def main():
     #   PLOTS
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
 
-    axes[0].set_title('Implementation')
+    axes[0].set_title('Observed and predicted values')
     axes[0].plot(prediction, c='red')
+    axes[0].plot(sk_prediction, c='green')
     axes[0].scatter(range(0, len(y_test)), y_test)
-    
-    axes[1].set_title('Sklearn')
-    axes[1].plot(sk_prediction, c='red')
-    axes[1].scatter(range(0, len(y_test)), y_test)
+    axes[0].legend(('prediction','sklearn prediction','observed'))
+    axes[0].set_xlabel('Index')
+    axes[0].set_ylabel('value')
 
+    errors, sk_errors = [], []
+    for i in range(0, len(prediction)):
+        errors.append(abs(prediction[i] - y_test[i]))
+        sk_errors.append(abs(sk_prediction[i] - y_test[i]))
+    
+    plt.style.use('seaborn-deep')
+    bins = np.linspace(0, 10, 30)
+    axes[1].hist([errors, sk_errors], bins, label=['x', 'y'])
+    axes[1].set_title('Histogram of residuals of observed and predicted values')
+    axes[1].legend(('implementation','sklearn'))
+    axes[1].set_xlabel('value')
+    axes[1].set_ylabel('quantity')
+    
+    axes[0].set_position((.07, .20, .4, .65))
+    axes[1].set_position((.55, .20, .4, .65))
+    text = "Learning rate: " + str(LEARNING_RATE) + ", k-fold: " + str(K_FOLD) + ", iterations: " + str(M) + ", tree depth: " + str(TREE_DEPTH)
+    plt.figtext(.5, .05, text , horizontalalignment ="center")
     plt.savefig('plot.png')
+
+    values = calculate_parameters(y_test, prediction)
+    values.append(execution_time)
+    sk_values = calculate_parameters(y_test, sk_prediction)
+    sk_values.append(sk_execution_time)
+    df = pd.DataFrame({'projekt': values, 'sklearn': sk_values}) 
+    df.to_excel('parameters.xlsx')
+
+
 
     print('My: ' + str(execution_time) + ', Sklearn: ' + str(sk_execution_time))
 
--- tree.py
+++ tree.py
@@ -1,7 +1,7 @@
 import statistics
 import timeit
 
-THRESHOLD_STEP = 5
+THRESHOLD_STEPS = 10
 
 class Leaf:
     def __init__(self, y):
@@ -25,13 +25,14 @@ class Question:
 
 def find_best_split(x, y):
     best_value = float("inf")
-    best_question = None 
+    best_question = Question(0, 0)
     n_features = len(x[0])
 
     for col in range(n_features):  # for each feature
         x_sorted, y_sorted = sort_column(x, y, col)
         current_value, threshold = best_threshold(x_sorted, y_sorted)
-        if threshold >= max(x_sorted):
+
+        if(threshold > x_sorted[-1]):
             continue
 
         # print('Column: ' + str(col) + ', SSR: ' + str(current_value) + ', Threshold: ' + str(threshold))
@@ -46,21 +47,52 @@ def find_best_split(x, y):
 def best_threshold(x, y):
     lowest_sum = float('inf')
     threshold = 0
-    for i in range(1, len(y), int(len(y) / THRESHOLD_STEP)):
-        avg_before_threshold = statistics.mean(y[:i])
-        avg_after_threshold = statistics.mean(y[i:])
+
+    steps = calculate_steps(x)
+
+    for i in steps:
+        avg_before, avg_after = calculate_averages(x, y, i)
         sum_of_residuals = 0
         for j in range(0, len(y)):
-            if j < i:
-                sum_of_residuals += ((y[j] - avg_before_threshold) ** 2)
+            if x[j] < i:
+                sum_of_residuals += ((y[j] - avg_before) ** 2)
             else:
-                sum_of_residuals += ((y[j] - avg_after_threshold) ** 2)
+                sum_of_residuals += ((y[j] - avg_after) ** 2)
 
         if sum_of_residuals < lowest_sum: 
             lowest_sum = sum_of_residuals
-            threshold = x[i]
+            threshold = i
     
     return lowest_sum, threshold
+
+def calculate_steps(x):
+    step = (x[-1] - x[0]) / THRESHOLD_STEPS + 1
+    steps = []
+    value = x[0]
+    while value < x[-1]:
+        value += step
+        steps.append(value)
+
+    return steps
+
+def calculate_averages(x, y, threshold):
+    avg_before = 0
+    before_id = 0
+    avg_after = 0
+    after_id = 0
+    for i in range(0, len(y)):
+        if x[i] < threshold:
+            avg_before += x[i]
+            before_id += 1
+        else:
+            avg_after += x[i]
+            after_id += 1
+    if before_id != 0:
+        avg_before /= before_id
+    if after_id != 0:
+        avg_after /= after_id
+
+    return avg_before, avg_after
 
 def build_tree(x, y, depth, MAX_LEAVES):
     if depth <= 0 or len(y) < MAX_LEAVES:
@@ -68,6 +100,8 @@ def build_tree(x, y, depth, MAX_LEAVES):
     depth -= 1
 
     question = find_best_split(x, y)
+
+    # print('Question: ' + str(question.column) + ' ' + str(question.value))
 
     true_x, true_y, false_x, false_y = partition(x, y, question)
 
